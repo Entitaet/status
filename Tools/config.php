c6e014deb09f9859f47829fc78aad2bc82fcbeb6
seeseekey <seeseekey@gmail.com>
2014-04-06 18:45:53 +0200

    Add tools to repository.

---
+++ Tools/config.php
@@ -0,0 +1,7 @@
+<?php
+  //Database
+  $dbHost = 'localhost'; // Datenbank Server - meistens "localhost" kann aber auch eine IP sein
+  $dbName = 'CHANGEME'; // Datenbank Name
+  $dbUser = 'CHANGEME'; // Datenbank Benutzer
+  $dbPassword = 'CHANGEME'; // Datenbank Passwort  
+?>
