fed2fd69e8bd31446f7307c7ef399d1de3162fec
seeseekey <seeseekey@gmail.com>
2014-04-06 22:12:06 +0200

    Better database connection detection and CSS for Images.

--- Frontends/SignalLight/status.php
+++ Frontends/SignalLight/status.php
@@ -6,47 +6,50 @@
 	$serviceOnline = true;
 
 	//Datenbank
-	$db = @mysql_connect ($dbHost, $dbUser, $dbPassword) or die ("Es konnte keine Verbindung zum Datenbankserver hergestellt werden");
+	$db = @mysql_connect ($dbHost, $dbUser, $dbPassword) or $serviceOnline=false;
 
-	//Datenbank auf UTF8 setzen
-	mysql_query("SET NAMES 'UTF8'");
-
-	// Datenbank auswählen
-	mysql_select_db ($dbName, $db) or die("Die Datenbank \"$name\" konnte nicht ausgew&auml;hlt werden");
-
-	$sql = "SELECT * FROM status WHERE `key`='entities'";
-
-	$result = mysql_query($sql);
-	
-	$countPersons=0;
-	
-	if($result!=false)
+	if($serviceOnline==true)
 	{
-		$row = mysql_fetch_array($result);
-		$countPersons=intval($row['value']);
-	}
-	else
-	{
-		$service_online=false;
-	}
+		//Datenbank auf UTF8 setzen
+		mysql_query("SET NAMES 'UTF8'");
 
-	//Datenbank schließen
-	mysql_close($db);
+		// Datenbank auswählen
+		mysql_select_db ($dbName, $db) or die("Die Datenbank \"$name\" konnte nicht ausgew&auml;hlt werden");
+
+		$sql = "SELECT * FROM status WHERE `key`='entities'";
+
+		$result = mysql_query($sql);
+	
+		$countPersons=0;
+	
+		if($result!=false)
+		{
+			$row = mysql_fetch_array($result);
+			$countPersons=intval($row['value']);
+		}
+		else
+		{
+			$service_online=false;
+		}
+
+		//Datenbank schließen
+		mysql_close($db);
+	}
 
 	//Ausgabe
 	if($serviceOnline==true)
 	{
 		if($countPersons>0)
 		{
-			echo '<img src="' . $statusURL . '/img/green.svg" style="padding-bottom:12px;height:300px;"/><br/>Im Moment ist der Hackerspace besetzt und kann besucht werden.';
+			echo '<img class="statusImage" src="' . $statusURL . '/img/green.svg"/><div>Im Moment ist der Hackerspace besetzt und kann besucht werden.</div>';
 		}
 		else
 		{
-			echo '<img src="' . $statusURL . '/img/red.svg" style="padding-bottom:12px;height:300px;"/><br/>Im Moment ist der Hackerspace leider nicht besetzt.';
+			echo '<img class="statusImage" src="' . $statusURL . '/img/red.svg"/><div>Im Moment ist der Hackerspace leider nicht besetzt.</div>';
 		}
 	}
 	else
 	{
-		echo '<img src="' . $statusURL . '/img/yellow.svg" style="padding-bottom:12px;height:300px;"/><br/>Im Moment kann der Status des Hackerspaces nicht ermittelt werden.';
+		echo '<img class="statusImage" src="' . $statusURL . '/img/yellow.svg"/><div>Im Moment kann der Status des Hackerspaces nicht ermittelt werden.</div>';
 	}
 ?>
