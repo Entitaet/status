5b626f5446c8b81c3994eb92dbf98445373a3491
seeseekey <seeseekey@Lilith.local>
2014-07-02 17:18:02 +0200

    Switch fronted SignalLight to REST-API.

--- Frontends/SignalLight/config.php
+++ Frontends/SignalLight/config.php
@@ -1,9 +1,6 @@
 <?php
   $statusURL="http://status.entitaet.org/";
   
-  //Database
-  $dbHost = 'localhost'; // Datenbank Server - meistens "localhost" kann aber auch eine IP sein
-  $dbName = 'CHANGEME'; // Datenbank Name
-  $dbUser = 'CHANGEME'; // Datenbank Benutzer
-  $dbPassword = 'CHANGEME'; // Datenbank Passwort  
+  $token = 'geheim'; // Access token
+  $apiURL = 'http://api.entitaet.org/status/'; // URL
 ?>
--- Frontends/SignalLight/status.php
+++ Frontends/SignalLight/status.php
@@ -1,40 +1,14 @@
 <?php
-
+	require_once("rest.php");
 	require_once("config.php");
 
 	//Vars
 	$serviceOnline = true;
 
-	//Datenbank
-	$db = @mysql_connect ($dbHost, $dbUser, $dbPassword) or $serviceOnline=false;
-
-	if($serviceOnline==true)
-	{
-		//Datenbank auf UTF8 setzen
-		mysql_query("SET NAMES 'UTF8'");
-
-		// Datenbank auswählen
-		mysql_select_db ($dbName, $db) or die("Die Datenbank \"$name\" konnte nicht ausgew&auml;hlt werden");
-
-		$sql = "SELECT * FROM status WHERE `key`='entities'";
-
-		$result = mysql_query($sql);
+	$url=$apiURL . "entities/?token=" . $token;
+	$ret=CallRESTAPI("GET", $url);
 	
-		$countPersons=0;
-	
-		if($result!=false)
-		{
-			$row = mysql_fetch_array($result);
-			$countPersons=intval($row['value']);
-		}
-		else
-		{
-			$service_online=false;
-		}
-
-		//Datenbank schließen
-		mysql_close($db);
-	}
+	$countPersons=(int)$ret;
 
 	//Ausgabe
 	if($serviceOnline==true)
