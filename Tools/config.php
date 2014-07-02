2b5994a799369e9a70249499947a5d0d59e7a2f1
seeseekey <seeseekey@Lilith.local>
2014-07-02 17:07:38 +0200

    Switch tools to REST API.

--- Tools/config.php
+++ Tools/config.php
@@ -1,7 +1,4 @@
 <?php
-  //Database
-  $dbHost = 'localhost'; // Datenbank Server - meistens "localhost" kann aber auch eine IP sein
-  $dbName = 'CHANGEME'; // Datenbank Name
-  $dbUser = 'CHANGEME'; // Datenbank Benutzer
-  $dbPassword = 'CHANGEME'; // Datenbank Passwort  
+  $token = 'geheim'; // Access token
+  $apiURL = 'http://api.entitaet.org/status/'; // URL
 ?>
--- Tools/setEntityStateToOne.php
+++ Tools/setEntityStateToOne.php
@@ -1,19 +1,7 @@
 <?php
-
+	require_once("rest.php");
 	require_once("config.php");
-
-	//Datenbank
-	$db = @mysql_connect ($dbHost, $dbUser, $dbPassword) or die ("Es konnte keine Verbindung zum Datenbankserver hergestellt werden");
-
-	//Datenbank auf UTF8 setzen
-	mysql_query("SET NAMES 'UTF8'");
-
-	// Datenbank auswählen
-	mysql_select_db ($dbName, $db) or die("Die Datenbank \"$name\" konnte nicht ausgew&auml;hlt werden");
-
-	$sql = "UPDATE status SET value = '1'  WHERE `key`='entities'";
-	mysql_query($sql);
-
-	//Datenbank schließen
-	mysql_close($db);
+	
+	$url=$apiURL . "entities/1/?token=" . $token;
+	CallRESTAPI("POST", $url);
 ?>
